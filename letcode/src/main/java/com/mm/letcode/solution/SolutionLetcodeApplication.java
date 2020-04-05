05cef27da58166590b76e64cc3869a1f74aaaeb9
fujie feng <fujie.feng@qunar.com>
2020-04-05 18:51:38 +0800

    commit

---
+++ letcode/src/main/java/com/mm/letcode/solution/SolutionLetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.mm.letcode.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionLetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionLetcodeApplication.class, args);
+	}
+
+}
---
+++ letcode/src/main/java/com/mm/letcode/sort/ISort.java
@@ -0,0 +1,6 @@
+package com.mm.letcode.sort;
+
+public interface ISort {
+
+    int[] sort(int[] nums);
+}
